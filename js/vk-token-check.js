a0f20a37fb5da551c2ddc48f7ef550fe37aa030a
Andrey Bozin <andrei_bozin@mail.ru>
2026-04-03 02:32:43 +0500

    change ui

--- js/vk-token-check.js
+++ js/vk-token-check.js
@@ -9,3 +9,15 @@ if (VK_STORAGE_TOKEN_ITEM_NAME in localStorage) {
 		isVkTokenActive = true
 	}
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tokenAlert = document.querySelector('.alert-no-vk-token');
+    
+    if (tokenAlert) {
+        if (!isVkTokenActive) {
+            tokenAlert.classList.remove('d-none');
+        } else {
+            tokenAlert.classList.add('d-none');
+        }
+    }
+});
